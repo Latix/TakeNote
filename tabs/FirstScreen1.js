0fb0ddb3586fb83670db9f9d01b8c3d8f6e9bb84
kamsi <kkodi@seamfix.com>
2018-07-28 12:27:19 +0100

    Test Run

--- tabs/FirstScreen1.js
+++ tabs/FirstScreen1.js
@@ -1,6 +1,6 @@
 import React, {Component } from 'react';
 import { View, Text,Image } from 'react-native';
-import { Card, Button } from 'react-native-material-design';
+//import { Card, Button } from 'react-native-material-design';
 
 // const util = require('util');
 
@@ -18,31 +18,31 @@ export default class FirstScreen1 extends Component {
     render() {
        
         return (
-            <View>
-            <Card>
-                <Card.Media
-                    image={<Image source={require('../assets/note.jpg')} />}
-                    overlay
-                />
-                <Card.Body>
-                    <Text>Some text to go in the body.</Text>
-                </Card.Body>
-                <Card.Actions position="right">
-                    <Button value="ACTION" />
-                </Card.Actions>
-            </Card>
-        </View>
-            // <View style={
-            //     {
-            //         flex: 1,
-            //         justifyContent: 'center',
-            //         alignItems: 'center'
-            //     }
-            // }>
-            //    <Text style={{fontSize:30}}>
-            //         This is tab 1
-            //    </Text>
-            // </View>
+        //     <View>
+        //     <Card>
+        //         <Card.Media
+        //             image={<Image source={require('../assets/icons/note.jpg')} />}
+        //             overlay
+        //         />
+        //         <Card.Body>
+        //             <Text>Some text to go in the body.</Text>
+        //         </Card.Body>
+        //         <Card.Actions position="right">
+        //             <Button value="ACTION" />
+        //         </Card.Actions>
+        //     </Card>
+        // </View>
+            <View style={
+                {
+                    flex: 1,
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }
+            }>
+               <Text style={{fontSize:30}}>
+                    This is tab 1
+               </Text>
+            </View>
         );
     }
 }
