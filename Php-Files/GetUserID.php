47af99016736cc31d0d181b05e65eca12d1f2488
kamsi kodi <kamsikodi@gmail.com>
2018-08-16 10:03:13 +0000

    Upload New File

---
+++ Php-Files/GetUserID.php
@@ -0,0 +1,41 @@
+<?php
+include 'DBConfig.php';
+ 
+// Create connection
+$conn = new mysqli($HostName, $HostUser, $HostPass, $DatabaseName);
+ 
+if ($conn->connect_error) {
+ 
+ die("Connection failed: " . $conn->connect_error);
+} 
+ // Getting the received JSON into $json variable.
+ $json = file_get_contents('php://input');
+ 
+ // decoding the received JSON and store into $obj variable.
+ $obj = json_decode($json,true);
+ 
+ // Populate UserName from JSON $obj array and store into $Note Id.
+ $userName = $obj['UserName'];
+ 
+// Creating SQL command to fetch all records from Table.
+$sql = "SELECT * FROM users WHERE userName='$userName'";
+ 
+$result = $conn->query($sql);
+ 
+if ($result->num_rows >0) {
+ 
+ 
+ while($row[] = $result->fetch_assoc()) {
+ 
+ $item = $row;
+ 
+ $json = json_encode($item);
+ 
+ }
+ 
+} else {
+ echo "No Results Found.";
+}
+ echo $json;
+$conn->close();
+?>
